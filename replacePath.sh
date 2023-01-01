0484f75693be1171e5d36e5cc9c151097a5a117c
masao.setogawa <ms.job.hunting@gmail.com>
2023-01-01 21:39:13 +0900

    ビルド

--- replacePath.sh
+++ replacePath.sh
@@ -1,7 +1,9 @@
 echo "<<replace to deploy for github>>"
-grep -l 'javascripts' ./docs/*.html | xargs sed -i '' -e 's/javascripts/ARPR-Page\/javascripts/g'
-grep -l 'stylesheets' ./docs/*.html | xargs sed -i '' -e 's/stylesheets/ARPR-Page\/stylesheets/g'
-grep -l 'images' ./docs/*.html | xargs sed -i '' -e 's/images/ARPR-Page\/images/g'
-grep -l 'images' ./docs/stylesheets/*.css | xargs sed -i '' -e 's/images/ARPR-Page\/images/g'
-grep -l 'index.html' ./docs/*.html | xargs sed -i '' -e 's/index\.html/ARPR-Page\/index.html/g'
-grep -l 'privacypolicy.html' ./docs/*.html | xargs sed -i '' -e 's/privacypolicy.html/ARPR-Page\/privacypolicy.html/g'
+directory="SETO-LAB"
+echo "s/javascripts/$directory\/javascripts/g"
+grep -l 'javascripts' ./docs/*.html | xargs sed -i '' -e 's/javascripts/$directory\/javascripts/g'
+grep -l 'stylesheets' ./docs/*.html | xargs sed -i '' -e 's/stylesheets/$directory\/stylesheets/g'
+grep -l 'images' ./docs/*.html | xargs sed -i '' -e 's/images/$directory\/images/g'
+grep -l 'images' ./docs/stylesheets/*.css | xargs sed -i '' -e 's/images/$directory\/images/g'
+grep -l 'index.html' ./docs/*.html | xargs sed -i '' -e 's/index\.html/$directory\/index.html/g'
+grep -l 'privacypolicy.html' ./docs/*.html | xargs sed -i '' -e 's/privacypolicy.html/$directory\/privacypolicy.html/g'
