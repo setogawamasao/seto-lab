82e7820e799b1f6ed8eb64234db5f671f23fa95f
masao.setogawa <ms.job.hunting@gmail.com>
2023-01-01 12:33:45 +0900

    スキル欄を追加、タイトル画像を追加、フッターを修正、コピーライトを修正

--- webpack.config.js
+++ webpack.config.js
@@ -1,113 +1,113 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    devtool: 'source-map',
-    entry: './src/javascripts/main.js',
-    output: {
-        path: path.resolve(__dirname, './docs'),
-        filename: 'javascripts/[name]-[contenthash].js',
-        publicPath: '/',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'ts-loader',
-                    },
-                ],
-            },
-            {
-                test: /\.js/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: [
-                                ['@babel/preset-env', { 'targets': '> 0.25%, not dead'}],
-                                '@babel/preset-react',
-                            ],
-                        },
-                    },
-                ],
-            },
-            {
-                test: /\.(css|sass|scss)/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            sourceMap: false,
-                        },
-                    },
-                    {
-                        loader: 'sass-loader',
-                    },
-                ],
-            },
-            {
-                test: /\.(png|jpg|jpeg|mp4|svg)/,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'images/[name]-[contenthash][ext]',
-                },
-                use: [
-                    // {
-                    //     loader: 'file-loader',
-                    //     options: {
-                    //         esModule: false,
-                    //         name: 'images/[name].[ext]',
-                    //     }
-                    // },
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            mozjpeg: {
-                                progressive: true,
-                                quality: 65,
-                            },
-                        },
-                    },
-                ],
-            },
-            {
-                test: /\.pug/,
-                use: [
-                    {
-                        loader: 'html-loader',
-                    },
-                    {
-                        loader: 'pug-html-loader',
-                        options: {
-                            pretty: true,
-                        }
-                    }
-                ]
-            }
+  mode: "development",
+  devtool: "source-map",
+  entry: "./src/javascripts/main.js",
+  output: {
+    path: path.resolve(__dirname, "./docs"),
+    filename: "javascripts/[name]-[contenthash].js",
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader",
+          },
         ],
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: './stylesheets/[name]-[contenthash].css',
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/templates/index.pug',
-            filename: 'index.html',
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/templates/privacypolicy.pug',
-            filename: 'privacypolicy.html',
-        }),
-        new CleanWebpackPlugin(),
+      },
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                ["@babel/preset-env", { targets: "> 0.25%, not dead" }],
+                "@babel/preset-react",
+              ],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(css|sass|scss)/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: false,
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|jpeg|mp4|svg|gif)/,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name]-[contenthash][ext]",
+        },
+        use: [
+          // {
+          //     loader: 'file-loader',
+          //     options: {
+          //         esModule: false,
+          //         name: 'images/[name].[ext]',
+          //     }
+          // },
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.pug/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+          {
+            loader: "pug-html-loader",
+            options: {
+              pretty: true,
+            },
+          },
+        ],
+      },
     ],
-}
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "./stylesheets/[name]-[contenthash].css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/templates/index.pug",
+      filename: "index.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/templates/privacypolicy.pug",
+      filename: "privacypolicy.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
