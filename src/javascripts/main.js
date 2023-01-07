d5d4c70cf0968c68317d26b0348e31cd3756fcb3
masao.setogawa <ms.job.hunting@gmail.com>
2023-01-07 15:59:32 +0900

    グリッチエフェクトを追加

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -6,3 +6,30 @@ import "../images/meta/favicon-32x32.png";
 import "../images/meta/favicon-16x16.png";
 import "../images/meta/site.webmanifest";
 import "../images/meta/safari-pinned-tab.svg";
+
+const p5 = require("p5");
+let isLoaded = false;
+let glitch;
+let playful;
+import imgSrc from "../images/top/main_title.png";
+import Glitch from "./glitch";
+
+function initP5(p) {
+  p.setup = function () {
+    const canvas = p.createCanvas(673, 131);
+    canvas.parent("P5Canvas");
+    p.loadImage(imgSrc, function (img) {
+      glitch = new Glitch(img, p);
+      isLoaded = true;
+    });
+  };
+
+  p.draw = function () {
+    p.clear();
+    if (isLoaded) {
+      glitch.show();
+    }
+  };
+}
+
+const app = new p5(initP5, document.body);
